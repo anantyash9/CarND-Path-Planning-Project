447126605648866f4ba535e5f4d805aca779f2f4
Anant Yash Pande <anantyash9@gmail.com>
2018-05-11 01:40:25 +0530

    please!

--- src/main.cpp
+++ src/main.cpp
@@ -201,7 +201,7 @@ int main() {
   	map_waypoints_dy.push_back(d_y);
   }
   int lane = 1;
-  double ref_vel = 0.0;
+  double ref_vel = 0.1;
 
   h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy,&lane,&ref_vel](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
